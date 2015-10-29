f42b158d0b38535c6d1f77fe26e8c90e183b4554
Ross Hamilton <rosshamilton.dev@gmail.com>
2015-10-29 01:27:28 +0000

    first commit

---
+++ roman_numerals/app/views/roman.rb
@@ -0,0 +1,20 @@
+
+@M = 1000
+@D = 500
+@C = 100
+@L = 50
+@X = 10
+@V = 5
+@I = 1
+
+@num = 0
+@r = roman.chars
+
+
+do if @r.!empty?
+  if r.first > || == r.all
+    @num + r.first
+    r.delete.0
+  else
+    @num - r.first
+    r.delete.0
